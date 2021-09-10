15cdc1f4c294d4be9697515c7efa2a1fe40db538
diamondpete <87245367+diamondpete@users.noreply.github.com>
2021-09-10 18:28:59 -0400

    Create siteNetVideoGirls.py

---
+++ Contents/Code/siteNetVideoGirls.py
@@ -0,0 +1,28 @@
+import PAsearchSites
+import PAgenres
+import PAactors
+import PAutils
+
+
+def search(results, encodedTitle, searchTitle, siteNum, lang, searchDate):
+    encodedTitle = searchTitle.lower().replace(' ', '-')
+    directURL = PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle + '.html'
+
+    searchResults = [directURL]
+    googleResults = PAutils.getFromGoogleSearch(searchTitle, siteNum)
+    for sceneURL in googleResults:
+        if '/trailers/' in sceneURL and sceneURL not in searchResults:
+            searchResults.append(sceneURL)
+
+    for sceneURL in searchResults:
+        req = PAutils.HTTPRequest(sceneURL)
+        detailsPageElements = HTML.ElementFromString(req.text)
+
+        titleNoFormatting = detailsPageElements.xpath('//h1')[0].text_content().strip()
+        curID = PAutils.Encode(sceneURL)
+
+        score = 100 - Util.LevenshteinDistance(searchTitle, titleNoFormatting)
+
+        results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [%s]' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum)), score=score, lang=lang))
+
+    return results
